68b3c8c8ab3f49de68a2c6fbb01a75bfff8fac93
Portablefire22 <barnowlsarecool@gmail.com>
2024-05-29 21:10:03 +0100

    Switched to using the SDL renderer instead of just using a surface Minimap now shows the direction that the player is moving Player rotation switched to using fixed point

--- main.c
+++ main.c
@@ -3,6 +3,7 @@
 #include <SDL2/SDL_events.h>
 #include <SDL2/SDL_keycode.h>
 #include <SDL2/SDL_rect.h>
+#include <SDL2/SDL_render.h>
 #include <SDL2/SDL_stdinc.h>
 #include <SDL2/SDL_surface.h>
 #include <SDL2/SDL_video.h>
@@ -11,8 +12,10 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-const int SCREEN_HEIGHT = 1080;
-const int SCREEN_WIDTH = 1920;
+#define SCREEN_HEIGHT 1080
+#define SCREEN_WIDTH 1920
+
+#define PI 3145
 
 const uint8_t map[6][8] = {
     {1,1,1,1,1,1,1,1},
@@ -31,7 +34,7 @@ const int MINIMAP_BLOCK_SIZE = 50 * (SCREEN_WIDTH / SCREEN_HEIGHT);
 typedef struct EntityStruct {
     float px; // Position X
     float py; // Position Y
-    float pr; // Rotation
+    int pr; // Rotation
     int msize; // Mini-map Size
     Uint32 col; // Entity colour
 } entity;
@@ -43,8 +46,11 @@ struct RayHit {
 
 struct {
     SDL_Window* window;
-    SDL_Surface* screen_surface;
-    Uint32* buffer;
+    SDL_Renderer* renderer;
+    SDL_Texture* texture;
+
+    Uint32 pixels[SCREEN_WIDTH * SCREEN_HEIGHT];
+
     entity** entity_list;
     entity* current_player;
     uint8_t entity_list_length;
@@ -61,23 +67,34 @@ int init() {
     if (engine.window == NULL) {
         printf("Window could not be created! SDL_Error: %s\n", SDL_GetError());
         return -1;
-    } 
+    }
 
-    engine.screen_surface = SDL_GetWindowSurface(engine.window);
+    engine.renderer = SDL_CreateRenderer(engine.window, -1, SDL_RENDERER_PRESENTVSYNC);
+    if (engine.renderer == NULL) {
+        return -1;
+    }
+
+    SDL_SetRenderDrawColor(engine.renderer, 0xff, 0xff, 0xff, 0xff);
+    
+    engine.texture = SDL_CreateTexture(engine.renderer, SDL_PIXELFORMAT_RGBA32, SDL_TEXTUREACCESS_STREAMING, SCREEN_WIDTH, SCREEN_HEIGHT);
+    if (engine.texture == NULL) {
+        return -1;
+    }
+
     return 0;
 }
 
 void draw_minimap() {
     int cx = 0;
     int cy = 0;
-
+    SDL_SetRenderDrawColor(engine.renderer, 0x0A, 0x0A, 0x0A, 0xff);
     for (int i=0; i < (int)(sizeof(map)/sizeof(map[0][0])); i++) {
         if (map[cy / MINIMAP_BLOCK_SIZE][cx / MINIMAP_BLOCK_SIZE]) {
             SDL_Rect* block = (SDL_Rect*)malloc(sizeof(SDL_Rect));
             block->h = block->w = MINIMAP_BLOCK_SIZE;
             block->x = cx;
             block->y = cy;
-            SDL_FillRect(engine.screen_surface, block, i);
+            SDL_RenderFillRect(engine.renderer, block);
             free(block);
         }
 
@@ -94,9 +111,15 @@ void draw_minimap() {
         box->w = box->h = ce->msize;
         box->x = ce->px;
         box->y = ce->py;
-        SDL_FillRect(engine.screen_surface, box, ce->col);
+        SDL_SetRenderDrawColor(engine.renderer, (ce->col >> 16) & 0xff, (ce->col >> 8) & 0xff, ce->col & 0xff, 0xff);
+        SDL_RenderFillRect(engine.renderer, box);
+        SDL_SetRenderDrawColor(engine.renderer, 0x00, 0xf0, 0xf0, 0xff);
+        int ty = (float)(ce->py + ce->msize / 2) + sin(ce->pr / 1000.0) * 50;
+        int tx = (float)(ce->px + ce->msize / 2) + cos(ce->pr / 1000.0) * 50;
+        SDL_RenderDrawLine(engine.renderer, ce->px + ce->msize / 2 , ce->py + ce->msize / 2, tx, ty);
         free(box);
     }
+    SDL_SetRenderDrawColor(engine.renderer, 0xff, 0xff, 0xff, 0xff);
 }
 
 void add_entity(entity* ent) {
@@ -113,7 +136,7 @@ void free_entities() {
     }
 }
 
-struct RayHit cast_ray(float ray_angle) {
+/*struct RayHit cast_ray(float ray_angle) {
     float px = engine.current_player->px;
     float py = engine.current_player->py;
     
@@ -164,7 +187,7 @@ void raycast() {
         struct RayHit ray_info = cast_ray(ray_angle);
         draw_wall_slice(i, &ray_info, slice_width);
     }
-}
+}*/
 
 int main(int argc, char* args[]) {
     
@@ -179,21 +202,16 @@ int main(int argc, char* args[]) {
     player->msize = 10;
     player->col = 0xff00ff;
     player->px = player->py = 3.0;
+    player->pr = PI;
     add_entity(player);
     
 
     engine.current_player = player;
     
-    printf("%d, %d\n",engine.entity_list_length, engine.entity_list[0]->msize);
-    
-    engine.buffer = (Uint32*) engine.screen_surface->pixels;
-
-    SDL_FillRect(engine.screen_surface, NULL, SDL_MapRGB(engine.screen_surface->format, 0xff, 0xff, 0xff));
-    SDL_UpdateWindowSurface(engine.window);
-
-
     SDL_Event e; 
     int quit = 0;
+    int i = 0;
+    int iter = 1;
     while (!quit) {
         while (SDL_PollEvent(&e)) {
             switch (e.type) {
@@ -202,31 +220,38 @@ int main(int argc, char* args[]) {
                     break;
                 case SDL_KEYDOWN:
                     switch (e.key.keysym.sym) {
-                        case SDLK_LEFT:
-                            engine.current_player->pr -= 0.1;
-                            break;
                         case SDLK_RIGHT:
-                            engine.current_player->pr += 0.1;
+                            engine.current_player->pr = engine.current_player->pr + (int) floorf(0.1 * PI);
+                            break;
+                        case SDLK_LEFT:
+                            engine.current_player->pr = engine.current_player->pr - (int) floorf(0.1 * PI);
                             break;
                         case SDLK_UP:
-                            engine.current_player->px++;
+                            engine.current_player->py += sin(engine.current_player->pr / 1000.0) * 5;
+                            engine.current_player->px += cos(engine.current_player->pr / 1000.0) * 5;
                             break;
                         case SDLK_DOWN:
-                            engine.current_player->py++;
+                            engine.current_player->py -= sin(engine.current_player->pr / 1000.0) * 5;
+                            engine.current_player->px -= cos(engine.current_player->pr / 1000.0) * 5;
                             break;
                     }
+                    break;
             }
         }
-        SDL_FillRect(engine.screen_surface, NULL, SDL_MapRGB(engine.screen_surface->format, 0xff, 0xff, 0xff));
-        raycast();
+        SDL_RenderClear(engine.renderer);
+        
+        SDL_RenderCopy(engine.renderer, engine.texture, NULL, NULL); // Render texture
         draw_minimap();
-        SDL_UpdateWindowSurface(engine.window);
+        SDL_RenderPresent(engine.renderer);
     }
         
     EXIT:
     free_entities();
-    SDL_FreeSurface(engine.screen_surface);
+    SDL_DestroyTexture(engine.texture);
+    SDL_DestroyRenderer(engine.renderer);
     SDL_DestroyWindow(engine.window);
+    engine.window = NULL;
+    engine.renderer = NULL;
     SDL_Quit();
 
     return 0;
